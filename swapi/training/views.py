794ae28b880072853dbadaf7b896f19a24f05ddf
Martin Zugnoni <martin.zugnoni@gmail.com>
2018-04-18 15:12:57 -0300

    Implement training

--- swapi/training/views.py
+++ swapi/training/views.py
@@ -11,7 +11,7 @@ def first_drf_view(request):
     """
     Return a simple JSON object.
     """
-    pass
+    return Response({"message": "Got a GET request!"})
 
 
 @api_view(['GET', 'POST'])
@@ -20,32 +20,55 @@ def multi_method_view(request):
     If received a POST request, return a JSON object displaying the submitted payloadself.
     Otherwise, return a simple JSON object showing the request method.
     """
-    pass
+    if request.method == 'POST':
+        return Response({
+            "message": "Got a POST request with this data: {}".format(request.data)})
+    return Response({"message": "Got a GET request!"})
 
 
 class FirstClassAPIView(APIView):
     """
     Implement support for GET requests.
     """
-    pass
+    def get(self, request):
+        return Response({
+            "message": "Got a GET request! This is a class-based APIView."})
 
 
 class MultiMethodAPIView(APIView):
     """
     Implement support for GET and POST requests.
     """
-    pass
+    def get(self, request):
+        return Response({"message": "Got a GET request!"})
+
+    def post(self, request):
+        return Response({
+            "message": "Got a POST request with this data: {}".format(self.request.data)})
 
 
 class CustomStatusCode(APIView):
     """
     Return a custom 400 BAD REQUEST status code.
     """
-    pass
+    def get(self, request):
+        content = {
+            "success": False,
+            "msg": "Invalid request, please check the documentation."
+        }
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomHeader(APIView):
     """
     Return a customer `X-RMOTR-IS-AWESOME` response header.
     """
-    pass
+    def get(self, request):
+        content = {
+            "success": True,
+            "msg": "Check the response headers to get a surprise ;-)"
+        }
+        headers = {
+            'X-RMOTR-IS-AWESOME': True
+        }
+        return Response(content, headers=headers)
